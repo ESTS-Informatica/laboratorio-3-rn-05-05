5bf5f1bb33c3f43511e4ac0f1906d59add7174a4
dianavn95 <202100637@estudantes.ips.pt>
2024-04-03 09:01:59 +0100

    Nível 1
    Implementação do nível 1 apenas com dúvidas na parte dos valores dos honorários

---
+++ AirTransportation.java
@@ -0,0 +1,20 @@
+
+/**
+ * Classe para representar um transporte áereo.
+ * 
+ * @author (Diana e Vitor) 
+ * @version (3/4/2024)
+ */
+public class AirTransportation extends Transport
+{
+    private String name; //atributo para o nome da aeronave
+    private int numberOfContainers; //atributo que identifica a quantidade de contentores
+    
+    /**
+     * Construtor para objetos da classe AirTransportation
+     */
+    public AirTransportation()
+    {
+        
+    }
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,19 @@
+
+/**
+ * Classe para representar um transporte terrestre.
+ * 
+ * @author (Diana e Vitor) 
+ * @version (3/4/2024)
+ */
+public class GroundTransportation extends Transport
+{
+    private String licensePlate; //atributo com a matrícula do veículo
+    
+    /**
+     * Construtor para objetos da classe GroundTransportation
+     */
+    public GroundTransportation()
+    {
+        
+    }
+}
